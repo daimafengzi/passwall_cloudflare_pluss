c5efe5f79706085046717a6f4a6360e01147608a
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2022-01-04 17:39:07 +0800

    Add files via upload

--- cf_start.sh
+++ cf_start.sh
@@ -24,7 +24,16 @@ anycast="${array[0]}"
 echo "最快的IP为：$anycast"
 
 #设置passwall的IP
-uci set passwall.1cc4384d183942aa9883e2425601489d.address=$anycast
+uci set passwall.36b59bcf1d5b41a2a89f87026ed48b85.address=$anycast
+uci set passwall.d9e86dca4e9f45a5bd393a12122072e9.address=${array[1]}
+uci set passwall.2a17ed2966e7474f8f46139711e9aee2.address=${array[2]}
+uci set passwall.a1d7615a501847ceb5e557ac68f9eb5f.address=${array[3]}
+uci set passwall.ec1add60d4f24816bb0a546d016679b8.address=${array[4]}
+uci set passwall.695cb8f679a046649b7b0f2c1dbd3426.address=${array[5]}
+uci set passwall.9ebeb8f7328e4e44ba99524f89f4cc3c.address=${array[6]}
+uci set passwall.aa3a370e953e47f290e65bde48cfb7eb.address=${array[7]}
+uci set passwall.1cc4384d183942aa9883e2425601489d.address=${array[8]}
+uci set passwall.ba86ec09658045118e8c333d5225cbdc.address=${array[9]}
 wait
 uci commit passwall
 
